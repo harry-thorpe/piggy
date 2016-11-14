c23b0e06b232ed07ec989fe9ffabbe95a039c3c2
Harry Thorpe <harry-thorpe@hotmail.co.uk>
2016-11-14 15:34:50 +0000

    add R plots option

--- bin/R_plotter.R
+++ bin/R_plotter.R
@@ -6,9 +6,10 @@ library(cowplot)
 library(reshape2)
 
 args <- commandArgs(trailingOnly=TRUE)
-args_count <- length(args)
 
 out_dir=args[1]
+threshold=args[2]
+threshold <- as.double(threshold)
 
 #####
 
@@ -21,7 +22,7 @@ switched_region_divergences_long <- melt(switched_region_divergences, id.vars="G
 
 sr_div_hist <- ggplot(switched_region_divergences_long, aes(x=value)) +
   geom_histogram(binwidth=0.005) +
-  geom_vline(xintercept=0.9, linetype="dashed", colour="red") +
+  geom_vline(xintercept=threshold, linetype="dashed", colour="red") +
   labs(x="Identity", y="Count") +
   facet_grid(variable~., scales="free")
 
@@ -37,8 +38,8 @@ dev.off()
 
 sr_div_point <- ggplot(switched_region_divergences, aes(x=Length_identity, y=Nuc_identity)) +
   geom_point(alpha=0.3) +
-  geom_hline(yintercept=0.9, linetype="dashed", colour="red") +
-  geom_vline(xintercept=0.9, linetype="dashed", colour="red") +
+  geom_hline(yintercept=threshold, linetype="dashed", colour="red") +
+  geom_vline(xintercept=threshold, linetype="dashed", colour="red") +
   labs(x="Length identity", y="Nucleotide identity")
 
 out_file=paste(out_dir, "/switched_region_divergences_point.tif", sep="")
@@ -51,16 +52,16 @@ dev.off()
 
 #####
 
-switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity < 0.9 & switched_region_divergences$Length_identity < 0.9), ]
+switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity < threshold & switched_region_divergences$Length_identity < threshold), ]
 Switched <- nrow(switched_region_divergences_subset)
 
-switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity < 0.9 & switched_region_divergences$Length_identity >= 0.9), ]
+switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity < threshold & switched_region_divergences$Length_identity >= threshold), ]
 Divergent <- nrow(switched_region_divergences_subset)
 
-switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity >= 0.9 & switched_region_divergences$Length_identity < 0.9), ]
+switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity >= threshold & switched_region_divergences$Length_identity < threshold), ]
 Insertion_Deletion <- nrow(switched_region_divergences_subset)
 
-switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity >= 0.9 & switched_region_divergences$Length_identity >= 0.9), ]
+switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity >= threshold & switched_region_divergences$Length_identity >= threshold), ]
 False_positive <- nrow(switched_region_divergences_subset)
 
 types_df <- data.frame(Insertion_Deletion, Switched, Divergent, False_positive, stringsAsFactors=FALSE)
@@ -89,7 +90,7 @@ cluster_IGR_divergences_long <- melt(cluster_IGR_divergences, measure.vars=c("Le
 
 within_IGR_cluster_divergence_plot <- ggplot(cluster_IGR_divergences_long, aes(x=Identity)) +
   geom_histogram(binwidth=0.001) +
-  geom_vline(xintercept=0.9, linetype="dashed", colour="red") +
+  geom_vline(xintercept=threshold, linetype="dashed", colour="red") +
   scale_x_continuous(limits=c(0.8,1.01)) +
   facet_grid(Category~.) +
   labs(y="Count")
@@ -108,8 +109,8 @@ dev.off()
 # igraph_test <- ggplot(switched_region_divergences, aes(x=Length_identity, y=Nuc_identity)) +
 #   geom_point() +
 #   geom_point_interactive(aes(data_id=Gene, tooltip=Gene)) +
-#   geom_hline(yintercept=0.9, linetype="dashed", colour="red") +
-#   geom_vline(xintercept=0.9, linetype="dashed", colour="red") +
+#   geom_hline(yintercept=threshold, linetype="dashed", colour="red") +
+#   geom_vline(xintercept=threshold, linetype="dashed", colour="red") +
 #   labs(x="Length identity", y="Nucleotide identity")
 # 
 # ggiraph(code=print(igraph_test), zoom_max=10)
