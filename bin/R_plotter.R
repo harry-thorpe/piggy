2f6726f605403cb9a02929f946ef4f55c4fb7052
Harry Thorpe <harry-thorpe@hotmail.co.uk>
2016-12-01 16:27:26 +0000

    change plotters

--- bin/R_plotter.R
+++ bin/R_plotter.R
@@ -1,8 +1,50 @@
 #!/usr/bin/Rscript
 
+## Gives count, mean, standard deviation, standard error of the mean, and confidence interval (default 95%).
+##   data: a data frame.
+##   measurevar: the name of a column that contains the variable to be summariezed
+##   groupvars: a vector containing names of columns that contain grouping variables
+##   na.rm: a boolean that indicates whether to ignore NA's
+##   conf.interval: the percent range of the confidence interval (default is 95%)
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval=.95, .drop=TRUE) {
+  library(plyr)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = mean   (xx[[col]], na.rm=na.rm),
+                     sd   = sd     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+  
+  # Rename the "mean" column    
+  datac <- rename(datac, c("mean" = measurevar))
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
+#####
+
 is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1]) 
 
-#####
 library(ggplot2)
 library(reshape2)
 
@@ -13,108 +55,122 @@ if(is.installed("cowplot") == TRUE){
 args <- commandArgs(trailingOnly=TRUE)
 
 out_dir=args[1]
-nuc_identity=args[2]
-nuc_identity <- as.double(nuc_identity)
-len_identity=args[2]
-len_identity <- as.double(len_identity)
+roary_dir=args[2]
 
 #####
 
-in_file=paste(out_dir, "/switched_region_divergences.csv", sep="")
+in_file=paste(roary_dir, "/gene_presence_absence.csv", sep="")
 
-switched_region_divergences <- read.csv(file=in_file, header=TRUE)
-#View(switched_region_divergences)
+gene_presence_absence <- read.csv(file=in_file, stringsAsFactors=FALSE, na.strings="", header=TRUE)
+#View(gene_presence_absence)
 
-switched_region_divergences_long <- melt(switched_region_divergences, id.vars="Gene", measure.vars=c("Length_identity", "Nuc_identity"))
+Category <- rep("Gene", nrow(gene_presence_absence))
 
-sr_div_hist <- ggplot(switched_region_divergences_long, aes(x=value)) +
-  geom_histogram(binwidth=0.005) +
-  labs(x="Identity", y="Count") +
-  facet_grid(variable~., scales="free")
+gene_presence_absence <- data.frame(Category, gene_presence_absence, stringsAsFactors=FALSE)
 
-out_file=paste(out_dir, "/switched_region_divergences_hist.tif", sep="")
+in_file=paste(out_dir, "/IGR_presence_absence.csv", sep="")
+
+IGR_presence_absence <- read.csv(file=in_file, stringsAsFactors=FALSE, na.strings="", header=TRUE)
+#View(IGR_presence_absence)
+
+Category <- rep("IGR", nrow(IGR_presence_absence))
+
+IGR_presence_absence <- data.frame(Category, IGR_presence_absence, stringsAsFactors=FALSE)
+
+#####
+
+all_presence_absence <- rbind(gene_presence_absence, IGR_presence_absence)
+
+all_freq_plot <- ggplot(all_presence_absence, aes(x=No..isolates, fill=Category)) +
+  geom_histogram(binwidth=1, position="dodge") +
+  labs(x="Isolates", y="Count")
+
+out_file=paste(out_dir, "/gene_IGR_frequency.tif", sep="")
 
 tiff(filename=out_file, height=5, width=10, units="in", res=100)
 
-sr_div_hist
+all_freq_plot
 
 dev.off()
 
 #####
 
-sr_div_point <- ggplot(switched_region_divergences, aes(x=Length_identity, y=Nuc_identity)) +
-  geom_point(alpha=0.3) +
-  geom_vline(xintercept=len_identity, linetype="dashed", colour="red") +
-  geom_hline(yintercept=nuc_identity, linetype="dashed", colour="red") +
-  labs(x="Length identity", y="Nucleotide identity")
+col_array <- seq(16, ncol(gene_presence_absence), 1)
+col_count <- length(col_array)
 
-out_file=paste(out_dir, "/switched_region_divergences_point.tif", sep="")
+row_array <- seq(1, nrow(gene_presence_absence), 1)
+row_count <- length(row_array)
 
-tiff(filename=out_file, height=5, width=10, units="in", res=100)
+Rep <- NULL
+Isolates <- NULL
+Count <- NULL
 
-sr_div_point
+count <- 0
+for(rep in 1:100){
+  col_array_resampled <- sample(col_array, replace=FALSE)
+  
+  rep_array <- rep(0, row_count)
+  for(col in 1:col_count){
+    
+    rep_array[ is.na(gene_presence_absence[,col_array_resampled[col]]) == FALSE ] <- 1
+    
+    count <- count + 1
+    Category[count] <- "Gene"
+    Rep[count] <- rep
+    Isolates[count] <- col
+    Count[count] <- sum(rep_array)
+  }
+}
 
-dev.off()
+gene_accumulation_df <- data.frame(Rep, Isolates, Count, stringsAsFactors=FALSE)
 
-#####
-
-switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity < nuc_identity & switched_region_divergences$Length_identity < len_identity), ]
-Switched <- nrow(switched_region_divergences_subset)
-
-switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity < nuc_identity & switched_region_divergences$Length_identity >= len_identity), ]
-Divergent <- nrow(switched_region_divergences_subset)
-
-switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity >= nuc_identity & switched_region_divergences$Length_identity < len_identity), ]
-Insertion_Deletion <- nrow(switched_region_divergences_subset)
-
-switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity >= nuc_identity & switched_region_divergences$Length_identity >= len_identity), ]
-False_positive <- nrow(switched_region_divergences_subset)
-
-types_df <- data.frame(Insertion_Deletion, Switched, Divergent, False_positive, stringsAsFactors=FALSE)
-
-types_df_long <- melt(types_df, variable.name="Category", value.name="Count")
-
-types_plot <- ggplot(types_df_long, aes(x=Category, y=Count)) +
-  geom_bar(stat="identity")
-
-out_file=paste(out_dir, "/switched_region_divergences_types.tif", sep="")
-
-tiff(filename=out_file, height=5, width=10, units="in", res=100)
-
-types_plot
-
-dev.off()
+gene_accumulation_df_summary <- summarySE(gene_accumulation_df, measurevar="Count", groupvars=c("Category", "Isolates"))
 
 #####
 
-in_file=paste(out_dir, "/cluster_IGR_divergences.csv", sep="")
+col_array <- seq(16, ncol(IGR_presence_absence), 1)
+col_count <- length(col_array)
 
-cluster_IGR_divergences <- read.csv(file=in_file, header=TRUE)
-#View(cluster_IGR_divergences)
+row_array <- seq(1, nrow(IGR_presence_absence), 1)
+row_count <- length(row_array)
 
-cluster_IGR_divergences_long <- melt(cluster_IGR_divergences, measure.vars=c("Length_identity", "Nuc_identity"), variable.name="Category", value.name="Identity")
+Rep <- NULL
+Isolates <- NULL
+Count <- NULL
 
-within_IGR_cluster_divergence_plot <- ggplot(cluster_IGR_divergences_long, aes(x=Identity)) +
-  geom_histogram(binwidth=0.001) +
-  facet_grid(Category~.) +
-  labs(y="Count")
+count <- 0
+for(rep in 1:100){
+  col_array_resampled <- sample(col_array, replace=FALSE)
+  
+  rep_array <- rep(0, row_count)
+  for(col in 1:col_count){
+    
+    rep_array[ is.na(IGR_presence_absence[,col_array_resampled[col]]) == FALSE ] <- 1
+    
+    count <- count + 1
+    Category[count] <- "IGR"
+    Rep[count] <- rep
+    Isolates[count] <- col
+    Count[count] <- sum(rep_array)
+  }
+}
 
-out_file=paste(out_dir, "/within_IGR_cluster_divergence_plot.tif", sep="")
+IGR_accumulation_df <- data.frame(Rep, Isolates, Count, stringsAsFactors=FALSE)
+
+IGR_accumulation_df_summary <- summarySE(IGR_accumulation_df, measurevar="Count", groupvars=c("Category", "Isolates"))
+
+#####
+
+all_accumulation_df_summary <- rbind(gene_accumulation_df_summary, IGR_accumulation_df_summary)
+
+all_accumulation_plot <- ggplot(all_accumulation_df_summary, aes(x=Isolates, y=Count, colour=Category, group=Category)) +
+  geom_line() +
+  scale_y_continuous(limits=c(0,3000))
+
+out_file=paste(out_dir, "/gene_IGR_accumulation.tif", sep="")
 
 tiff(filename=out_file, height=5, width=10, units="in", res=100)
 
-within_IGR_cluster_divergence_plot
+all_accumulation_plot
 
 dev.off()
-
-#####
-# library(ggiraph)
-# 
-# igraph_test <- ggplot(switched_region_divergences, aes(x=Length_identity, y=Nuc_identity)) +
-#   geom_point() +
-#   geom_point_interactive(aes(data_id=Gene, tooltip=Gene)) +
-#   geom_hline(yintercept=threshold, linetype="dashed", colour="red") +
-#   geom_vline(xintercept=threshold, linetype="dashed", colour="red") +
-#   labs(x="Length identity", y="Nucleotide identity")
-# 
-# ggiraph(code=print(igraph_test), zoom_max=10)
---
+++ bin/SR_plotter.R
@@ -0,0 +1,120 @@
+#!/usr/bin/Rscript
+
+is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1]) 
+
+#####
+library(ggplot2)
+library(reshape2)
+
+if(is.installed("cowplot") == TRUE){
+  library(cowplot)
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+
+out_dir=args[1]
+nuc_identity=args[2]
+nuc_identity <- as.double(nuc_identity)
+len_identity=args[2]
+len_identity <- as.double(len_identity)
+
+#####
+
+in_file=paste(out_dir, "/switched_region_divergences.csv", sep="")
+
+switched_region_divergences <- read.csv(file=in_file, header=TRUE)
+#View(switched_region_divergences)
+
+switched_region_divergences_long <- melt(switched_region_divergences, id.vars="Gene", measure.vars=c("Length_identity", "Nuc_identity"))
+
+sr_div_hist <- ggplot(switched_region_divergences_long, aes(x=value)) +
+  geom_histogram(binwidth=0.005) +
+  labs(x="Identity", y="Count") +
+  facet_grid(variable~., scales="free")
+
+out_file=paste(out_dir, "/switched_region_divergences_hist.tif", sep="")
+
+tiff(filename=out_file, height=5, width=10, units="in", res=100)
+
+sr_div_hist
+
+dev.off()
+
+#####
+
+sr_div_point <- ggplot(switched_region_divergences, aes(x=Length_identity, y=Nuc_identity)) +
+  geom_point(alpha=0.3) +
+  geom_vline(xintercept=len_identity, linetype="dashed", colour="red") +
+  geom_hline(yintercept=nuc_identity, linetype="dashed", colour="red") +
+  labs(x="Length identity", y="Nucleotide identity")
+
+out_file=paste(out_dir, "/switched_region_divergences_point.tif", sep="")
+
+tiff(filename=out_file, height=5, width=10, units="in", res=100)
+
+sr_div_point
+
+dev.off()
+
+#####
+
+switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity < nuc_identity & switched_region_divergences$Length_identity < len_identity), ]
+Switched <- nrow(switched_region_divergences_subset)
+
+switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity < nuc_identity & switched_region_divergences$Length_identity >= len_identity), ]
+Divergent <- nrow(switched_region_divergences_subset)
+
+switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity >= nuc_identity & switched_region_divergences$Length_identity < len_identity), ]
+Insertion_Deletion <- nrow(switched_region_divergences_subset)
+
+switched_region_divergences_subset <- switched_region_divergences[(switched_region_divergences$Nuc_identity >= nuc_identity & switched_region_divergences$Length_identity >= len_identity), ]
+False_positive <- nrow(switched_region_divergences_subset)
+
+types_df <- data.frame(Insertion_Deletion, Switched, Divergent, False_positive, stringsAsFactors=FALSE)
+
+types_df_long <- melt(types_df, variable.name="Category", value.name="Count")
+
+types_plot <- ggplot(types_df_long, aes(x=Category, y=Count)) +
+  geom_bar(stat="identity")
+
+out_file=paste(out_dir, "/switched_region_divergences_types.tif", sep="")
+
+tiff(filename=out_file, height=5, width=10, units="in", res=100)
+
+types_plot
+
+dev.off()
+
+#####
+
+in_file=paste(out_dir, "/cluster_IGR_divergences.csv", sep="")
+
+cluster_IGR_divergences <- read.csv(file=in_file, header=TRUE)
+#View(cluster_IGR_divergences)
+
+cluster_IGR_divergences_long <- melt(cluster_IGR_divergences, measure.vars=c("Length_identity", "Nuc_identity"), variable.name="Category", value.name="Identity")
+
+within_IGR_cluster_divergence_plot <- ggplot(cluster_IGR_divergences_long, aes(x=Identity)) +
+  geom_histogram(binwidth=0.001) +
+  facet_grid(Category~.) +
+  labs(y="Count")
+
+out_file=paste(out_dir, "/within_IGR_cluster_divergence_plot.tif", sep="")
+
+tiff(filename=out_file, height=5, width=10, units="in", res=100)
+
+within_IGR_cluster_divergence_plot
+
+dev.off()
+
+#####
+# library(ggiraph)
+# 
+# igraph_test <- ggplot(switched_region_divergences, aes(x=Length_identity, y=Nuc_identity)) +
+#   geom_point() +
+#   geom_point_interactive(aes(data_id=Gene, tooltip=Gene)) +
+#   geom_hline(yintercept=threshold, linetype="dashed", colour="red") +
+#   geom_vline(xintercept=threshold, linetype="dashed", colour="red") +
+#   labs(x="Length identity", y="Nucleotide identity")
+# 
+# ggiraph(code=print(igraph_test), zoom_max=10)
