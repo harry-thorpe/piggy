572bc0ffe9fe75b7241df98602b75dde81a66280
Harry Thorpe <harry-thorpe@hotmail.co.uk>
2017-06-14 15:48:34 +0100

    make cluster presence absence strict

--- bin/piggy_cluster_presence_absence.pl
+++ bin/piggy_cluster_presence_absence.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/env perl
 use warnings;
+use strict;
 
-$out_dir=$ARGV[0];
+my $out_dir=$ARGV[0];
 
 print STDOUT "Producing IGR presence absence matrix...\n";
 print STDERR "Producing IGR presence absence matrix...\n";
@@ -9,35 +10,40 @@ print STDERR "Producing IGR presence absence matrix...\n";
 open OUTPUT, ">$out_dir/IGR_presence_absence.csv";
 open OUTPUT_RTAB, ">$out_dir/IGR_presence_absence.Rtab";
 
+my @isolate_array=();
+
 open INPUT, "$out_dir/isolates.txt";
-while(<INPUT>){
-	$line=$_;
+while(my $line=<INPUT>){
 	chomp $line;
 	
 	push @isolate_array, $line;
 }
 close INPUT;
 
+my @cluster_array=();
+
 open INPUT, "$out_dir/clusters.txt";
-while(<INPUT>){
-	$line=$_;
+while(my $line=<INPUT>){
 	chomp $line;
 	
 	push @cluster_array, $line;
 }
 close INPUT;
 
-foreach $cluster(@cluster_array){
+my %cluster_isolate_count_hash=();
+my %cluster_seq_count_hash=();
+my %cluster_hash=();
+
+foreach my $cluster(@cluster_array){
 	open INPUT, "$out_dir/cluster_intergenic_files/$cluster.fasta";
-	while(<INPUT>){
-		$line=$_;
+	while(my $line=<INPUT>){
 		chomp $line;
 	
 		if($line =~ /^>(.+)/){
 			
-			$cluster_id=$1;
-			@cluster_id_array=split(/_\+_\+_/, $cluster_id);
-			$isolate=$cluster_id_array[0];
+			my $cluster_id=$1;
+			my @cluster_id_array=split(/_\+_\+_/, $cluster_id);
+			my $isolate=$cluster_id_array[0];
 		
 			if(!$cluster_hash{$cluster}{$isolate}){
 				$cluster_isolate_count_hash{$cluster}++;
@@ -52,23 +58,23 @@ foreach $cluster(@cluster_array){
 	}
 }
 
-@cluster_sorted_array=sort { $cluster_isolate_count_hash{$b} <=> $cluster_isolate_count_hash{$a} } keys %cluster_isolate_count_hash;
+my @cluster_sorted_array=sort { $cluster_isolate_count_hash{$b} <=> $cluster_isolate_count_hash{$a} } keys %cluster_isolate_count_hash;
 
 print OUTPUT "\"Gene\",\"Non-unique Gene name\",\"Annotation\",\"No. isolates\",\"No. sequences\",\"Avg sequences per isolate\",\"Genome Fragment\",\"Order within Fragment\",\"Accessory Fragment\",\"Accessory Order with Fragment\",\"QC\",\"Min group size nuc\",\"Max group size nuc\",\"Avg group size nuc\"";
 print OUTPUT_RTAB "Gene";
 
-foreach $isolate(@isolate_array){
+foreach my $isolate(@isolate_array){
 	print OUTPUT ",\"$isolate\"";
 	print OUTPUT_RTAB "\t$isolate";
 }
 print OUTPUT "\n";
 print OUTPUT_RTAB "\n";
 
-foreach $cluster(@cluster_sorted_array){
-	$ave_seqs=($cluster_seq_count_hash{$cluster}/$cluster_isolate_count_hash{$cluster});
+foreach my $cluster(@cluster_sorted_array){
+	my $ave_seqs=($cluster_seq_count_hash{$cluster}/$cluster_isolate_count_hash{$cluster});
 	
 	print OUTPUT "\"$cluster\",\"\",\"\",\"$cluster_isolate_count_hash{$cluster}\",\"$cluster_seq_count_hash{$cluster}\",\"$ave_seqs\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\"";
-	foreach $isolate(@isolate_array){
+	foreach my $isolate(@isolate_array){
 		if($cluster_hash{$cluster}{$isolate}){
 			print OUTPUT ",\"$cluster_hash{$cluster}{$isolate}\"";
 			print OUTPUT_RTAB "\t1";
--- bin/piggy_roary_piggy_combiner.pl
+++ bin/piggy_roary_piggy_combiner.pl
@@ -1,68 +1,72 @@
 #!/usr/bin/env perl
 use warnings;
+use strict;
 
 #use Text::CSV;
 
 #$csv = Text::CSV->new({ sep_char => ',', binary => 1 });
 
-$out_dir=$ARGV[0];
-$roary_dir=$ARGV[1];
-$method=$ARGV[2];
+my $out_dir=$ARGV[0];
+my $roary_dir=$ARGV[1];
+my $method=$ARGV[2];
 
 print STDOUT "Combining gene and IGR matrices...\n";
 print STDERR "Combining gene and IGR matrices...\n";
 
 open OUTPUT, ">$out_dir/roary_piggy_combined.tab";
 
+my @header_array=();
+my $col_count=0;
+my $isolate_sta=0;
+my $isolate_end=0;
+
+my %gene_id_hash=();
+			
 open INPUT_R, "$roary_dir/gene_presence_absence.csv";
-while(<INPUT_R>){
-	$line=$_;
+while(my $line=<INPUT>){
+	chomp $line;
 	$line=~s/\R//g;
 	$line=~s/^"//;
 	$line=~s/"$//;
-	@line_array=split(/","/, $line);
+	my @line_array=split(/","/, $line);
 	
 	#if($csv->parse($line)){
 		
 	#	@line_array=$csv->fields();
 		
-		if($line =~ /^Gene","/){
-			@header_array=@line_array;
-			
-			$col_count=scalar(@line_array);
+	if($line =~ /^Gene","/){
+		@header_array=@line_array;
 		
-			$isolate_sta=14;
-			$isolate_end=$col_count - 1;
+		$col_count=scalar(@line_array);
+	
+		$isolate_sta=14;
+		$isolate_end=$col_count - 1;
+	
+	}else{
+		# Only use single copy genes.
+		if($line_array[5] == 1){
+			for(my $i=$isolate_sta; $i<=$isolate_end; $i++){
+				if($line_array[$i]){
+					my $isolate=$header_array[$i];
+					my $gene_id=$line_array[$i];
+					my $gene=$line_array[0];
 		
-			for($i=$isolate_sta; $i<=$isolate_end; $i++){
-				#push @isolate_array, $header_array[$i];
-			}
-		}else{
-			# Only use single copy genes.
-			if($line_array[5] == 1){
-				for($i=$isolate_sta; $i<=$isolate_end; $i++){
-					if($line_array[$i]){
-						$isolate=$header_array[$i];
-						$gene_id=$line_array[$i];
-						$gene=$line_array[0];
-			
-						#print "$isolate\n";
-			
-						$gene_id_hash{$isolate}{$gene_id}=$gene;
-					}
+					$gene_id_hash{$isolate}{$gene_id}=$gene;
 				}
 			}
 		}
+	}
+	
 	#}
 }
 
 open INPUT_I, "$out_dir/IGR_presence_absence.csv";
-while(<INPUT_I>){
-	$line=$_;
+while(my $line=<INPUT>){
+	chomp $line;
 	$line=~s/\R//g;
 	$line=~s/^"//;
 	$line=~s/"$//;
-	@line_array=split(/","/, $line);
+	my @line_array=split(/","/, $line);
 	
 	if($line =~ /^Gene","/){
 		@header_array=@line_array;
@@ -72,31 +76,31 @@ while(<INPUT_I>){
 		$isolate_sta=14;
 		$isolate_end=$col_count - 1;
 		
-		#for($i=$isolate_sta; $i<=$isolate_end; $i++){
-		#	push @isolate_array, $header_array[$i];
-		#}
 	}else{
-		@int_gene_array=();
-		%int_gene_hash=();
-		for($i=$isolate_sta; $i<=$isolate_end; $i++){
+		my @int_gene_array=();
+		my %int_gene_hash=();
+		
+		for(my $i=$isolate_sta; $i<=$isolate_end; $i++){
 			if($line_array[$i]){
-				$isolate=$header_array[$i];
+				my $isolate=$header_array[$i];
+				my $int_id="";
 				if($line_array[$i] =~ /^(\S+)/){
 					$int_id=$1;
 				}
-				$int=$line_array[0];
-				
-				#$int_id_hash{$isolate}{$int_id}=$int;
+				my $int=$line_array[0];
 			
-				@int_id_array=split(/_\+_\+_/, $int_id);
+				my @int_id_array=split(/_\+_\+_/, $int_id);
 				
 				if($gene_id_hash{$isolate}{$int_id_array[1]} && $gene_id_hash{$isolate}{$int_id_array[2]}){
 				
-					$tmp_gene_1=$gene_id_hash{$isolate}{$int_id_array[1]};
-					$tmp_gene_2=$gene_id_hash{$isolate}{$int_id_array[2]};
+					my $tmp_gene_1=$gene_id_hash{$isolate}{$int_id_array[1]};
+					my $tmp_gene_2=$gene_id_hash{$isolate}{$int_id_array[2]};
 					
 					if($method eq "GENE_PAIR"){
-					
+						
+						my $tmp_gene_1="";
+						my $tmp_gene_2="";
+						
 						if($int_id_array[3] eq "CO_F"){
 							$tmp_gene_2="*$tmp_gene_2";
 						}elsif($int_id_array[3] eq "CO_R"){
@@ -106,15 +110,18 @@ while(<INPUT_I>){
 							$tmp_gene_2="*$tmp_gene_2";
 						}
 					
-						@tmp_gene_array=("$tmp_gene_1", "$tmp_gene_2");
-						@tmp_gene_array=sort(@tmp_gene_array);
+						my @tmp_gene_array=("$tmp_gene_1", "$tmp_gene_2");
+						my @tmp_gene_array=sort(@tmp_gene_array);
 		
-						$tmp_gene_1_2="$tmp_gene_array[0]_+_+_$tmp_gene_array[1]";
+						my $tmp_gene_1_2="$tmp_gene_array[0]_+_+_$tmp_gene_array[1]";
 		
 						$int_gene_hash{$tmp_gene_1_2}++;
 						
 					}elsif($method eq "UPSTREAM"){
-					
+						
+						my $tmp_gene_1="";
+						my $tmp_gene_2="";
+						
 						if($int_id_array[3] eq "CO_F"){
 							$tmp_gene_2="*$tmp_gene_2";
 						
@@ -137,7 +144,7 @@ while(<INPUT_I>){
 		
 		@int_gene_array=sort { $int_gene_hash{$b} <=> $int_gene_hash{$a} } keys %int_gene_hash;
 		
-		foreach $int_gene(@int_gene_array){
+		foreach my $int_gene(@int_gene_array){
 			print OUTPUT "$int\t$int_gene\t$int_gene_hash{$int_gene}\n";
 		}
 	}
