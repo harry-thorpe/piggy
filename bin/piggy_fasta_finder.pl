b6d612255b63cec98692b74ed6b0bb86d5b42029
Harry Thorpe <harry-thorpe@hotmail.co.uk>
2017-08-25 08:47:35 +0100

    reduce tmp files for IGR sequences

---
+++ bin/piggy_fasta_finder.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+my $search=$ARGV[0];
+my $in_file=$ARGV[1];
+
+my $name="";
+my $seq="";
+
+open INPUT, "$in_file" or die "Input file doesn't exist: $in_file\n";
+while(my $line=<INPUT>){
+	chomp $line;
+	
+	if($line =~ /^>(\S+)/){
+		$name=$1;
+	}elsif($line =~ /^([ATGCN-]+)/){
+		$seq=$1;
+		
+		if($name eq $search){
+			print $seq;
+		}
+	}
+}
+
--- bin/piggy_intergenic_sequence_extracter.pl
+++ bin/piggy_intergenic_sequence_extracter.pl
@@ -15,7 +15,6 @@ my $max_len=1000;
 my $max_n_prop=0.1;
 
 #open OUTPUT, ">>$out_dir/IGR_sequences.fasta" or die "Cannot open output file: $out_dir/IGR_sequences.fasta\n";
-open OUTPUT, ">$out_dir/${isolate}_IGR_sequences.fasta" or die "Cannot open output file: $out_dir/${isolate}_IGR_sequences.fasta\n";
 
 if(! -e "$out_iso_dir" && ! -d "$out_iso_dir"){
 	mkdir "$out_iso_dir" or die "Cannot create output folder: $out_iso_dir\n";
@@ -24,6 +23,9 @@ if(! -e "$out_iso_dir" && ! -d "$out_iso_dir"){
 if(! -e "$out_iso_g_dir" && ! -d "$out_iso_g_dir"){
 	mkdir "$out_iso_g_dir" or die "Cannot create output folder: $out_iso_g_dir\n";
 }
+
+open OUTPUT, ">$out_iso_dir/${isolate}_IGR_sequences.fasta" or die "Cannot open output file: $out_iso_dir/${isolate}_IGR_sequences.fasta\n";
+open OUTPUT_GF, ">$out_iso_g_dir/${isolate}_GF_sequences.fasta" or die "Cannot open output file: $out_iso_g_dir/${isolate}_GF_sequences.fasta\n";
 
 my @contig_array=();
 my %contig_hash=();
@@ -45,8 +47,12 @@ while(my $line=<INPUT>){
 			$contig_id=$1;
 			
 			push @contig_array, $contig_id;
-		}elsif($line =~ /^([ATGCN]+)/){
+		}elsif($line !~ /^>/ && $line =~ /^(\S+)/){
 			my $seq=$1;
+			$seq=~tr/atgcn/ATGCN/;
+			$seq=~tr/bdefhijklmopqrsuvwxyz/NNNNNNNNNNNNNNNNNNNNN/;
+			$seq=~tr/BDEFHIJKLMOPQRSUVWXYZ/NNNNNNNNNNNNNNNNNNNNN/;
+			$seq=~tr/-/N/;
 			
 			if(!$contig_hash{$contig_id}){
 				$contig_hash{$contig_id}=$seq;
@@ -92,9 +98,9 @@ foreach $contig(@contig_array){
 					if($n_prop < $max_n_prop){
 						print OUTPUT ">$int_id\n$int_seq\n";
 					
-						open OUTPUT_ISOLATE, ">$out_iso_dir/$int_id.fasta" or die "Cannot open output file: $out_iso_dir/$int_id.fasta\n";
-						print OUTPUT_ISOLATE ">$int_id\n$int_seq\n";
-						close OUTPUT_ISOLATE;
+						#open OUTPUT_ISOLATE, ">$out_iso_dir/$int_id.fasta" or die "Cannot open output file: $out_iso_dir/$int_id.fasta\n";
+						#print OUTPUT_ISOLATE ">$int_id\n$int_seq\n";
+						#close OUTPUT_ISOLATE;
 					}
 				}
 			}
@@ -136,10 +142,11 @@ foreach $contig(@contig_array){
 						my $base=substr($contig_seq, $x, 1);
 						$gene_e_seq=$gene_e_seq.$base;
 					}
+					print OUTPUT_GF ">${gene_id}_s\n$gene_s_seq\n>${gene_id}_e\n$gene_e_seq\n";
 				
-					open OUTPUT_ISOLATE, ">$out_iso_g_dir/$gene_id.fasta" or die "Cannot open output file: $out_iso_g_dir/$gene_id.fasta\n";
-					print OUTPUT_ISOLATE ">${gene_id}_s\n$gene_s_seq\n>${gene_id}_e\n$gene_e_seq\n";
-					close OUTPUT_ISOLATE;
+					#open OUTPUT_ISOLATE, ">$out_iso_g_dir/$gene_id.fasta" or die "Cannot open output file: $out_iso_g_dir/$gene_id.fasta\n";
+					#print OUTPUT_ISOLATE ">${gene_id}_s\n$gene_s_seq\n>${gene_id}_e\n$gene_e_seq\n";
+					#close OUTPUT_ISOLATE;
 				}
 			}
 		}
