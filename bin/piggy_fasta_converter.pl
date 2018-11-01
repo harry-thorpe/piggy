24d14e5efbac5a2947db97e7385e4bea7fc0f976
harry-thorpe <harry-thorpe@hotmail.co.uk>
2018-11-01 14:10:41 +0000

    fix bug in core IGR alignment creation

--- bin/piggy_fasta_converter.pl
+++ bin/piggy_fasta_converter.pl
@@ -2,8 +2,19 @@
 use warnings;
 use strict;
 
+# this script processes the output from mafft
+
 my $in_file=$ARGV[0];
 my $out_file=$ARGV[1];
+my $iso_file=$ARGV[2];
+
+my %iso_hash=();
+open ISO, "$iso_file" or die "Cannot open isolate file: $iso_file\n";
+while(my $line=<ISO>){
+    chomp $line;
+
+    $iso_hash{$line}=1;
+}
 
 open OUTPUT, ">$out_file" or die "Cannot open output file: $out_file\n";
 
@@ -12,18 +23,39 @@ open INPUT, "$in_file" or die "Input file doesn't exist: $in_file\n";
 while(my $line=<INPUT>){
 	chomp $line;
 	
-	if($line =~ /^>/){
+    if($line =~ /^>(\S+)/){
+        my $id=$1;
+
+        my @id_array=split(/_\+_\+_/, $id);
+        
+        my $iso=$id_array[0];
+        my $id_p="";
+
+        if($iso_hash{$iso}){
+            $id_p=$id;
+        }elsif($iso =~ /^_R_(\S+)/){
+            my $iso_rev=$1;
+            if($iso_hash{$iso_rev}){
+                $id_p="${iso_rev}_+_+_$id_array[1]_+_+_$id_array[2]_+_+_$id_array[3]_+_+_R";
+            }else{
+                die "ERROR: ID $iso_rev not recognised.\n";
+            }
+        }else{
+            die "ERROR: ID $iso not recognised.\n";
+        }
+
 		if($beg_seq == 0){
-			print OUTPUT "$line\n";
+			print OUTPUT ">$id_p\n";
 			$beg_seq=1;
 		}else{
-			print OUTPUT "\n$line\n";
+			print OUTPUT "\n>$id_p\n";
 		}
 	}else{
 		$line=~tr/a-z/A-Z/;
 		print OUTPUT "$line";
 	}
 }
-
 print OUTPUT "\n";
 
+close INPUT;
+close OUTPUT;
