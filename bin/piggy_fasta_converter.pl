bff0804821e940219fb74eadcc9e5c5820f44ce5
harry-thorpe <harry-thorpe@hotmail.co.uk>
2018-11-02 09:30:40 +0000

    improve fix

--- bin/piggy_fasta_converter.pl
+++ bin/piggy_fasta_converter.pl
@@ -4,52 +4,55 @@ use strict;
 
 # this script processes the output from mafft
 
+# aligned mafft file
 my $in_file=$ARGV[0];
+# desired output file
 my $out_file=$ARGV[1];
-my $iso_file=$ARGV[2];
+# initial fasta file which mafft aligned
+my $initial_in_file=$ARGV[2];
 
-my %iso_hash=();
-open ISO, "$iso_file" or die "Cannot open isolate file: $iso_file\n";
+my @initial_ids=();
+open ISO, "$initial_in_file" or die "Cannot open isolate file: $initial_in_file\n";
 while(my $line=<ISO>){
     chomp $line;
-
-    $iso_hash{$line}=1;
+    
+    if($line =~ />(\S+)/){
+        my $id=$1;
+        push @initial_ids, $id;
+    }
 }
+close ISO;
 
 open OUTPUT, ">$out_file" or die "Cannot open output file: $out_file\n";
 
-my $beg_seq=0;
+my $seq_count=0;
 open INPUT, "$in_file" or die "Input file doesn't exist: $in_file\n";
 while(my $line=<INPUT>){
 	chomp $line;
 	
     if($line =~ /^>(\S+)/){
         my $id=$1;
-
-        my @id_array=split(/_\+_\+_/, $id);
-        
-        my $iso=$id_array[0];
+        my $initial_id=$initial_ids[$seq_count];
         my $id_p="";
 
-        if($iso_hash{$iso}){
+        if($id eq $initial_id){
             $id_p=$id;
-        }elsif($iso =~ /^_R_(\S+)/){
-            my $iso_rev=$1;
-            if($iso_hash{$iso_rev}){
-                $id_p="${iso_rev}_+_+_$id_array[1]_+_+_$id_array[2]_+_+_$id_array[3]_+_+_R";
-            }else{
-                die "ERROR: ID $iso_rev not recognised.\n";
-            }
         }else{
-            die "ERROR: ID $iso not recognised.\n";
+            my $id_m="_R_$initial_id";
+            if($id eq $id_m){
+                $id_p="${initial_id}_+_+_R";
+            }else{
+                die "ERROR: IDs from initial mafft file and aligned file don't match: $initial_id\t$id\n";
+            }
         }
 
-		if($beg_seq == 0){
+        if($seq_count == 0){
 			print OUTPUT ">$id_p\n";
-			$beg_seq=1;
 		}else{
 			print OUTPUT "\n>$id_p\n";
 		}
+
+        $seq_count++;
 	}else{
 		$line=~tr/a-z/A-Z/;
 		print OUTPUT "$line";
